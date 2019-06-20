d604047d57f708d81d75a5e1bcd63170ee8dd6ea
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-06-20 23:41:59 +0200

    Menu User

--- src/pl/coderslab/menu/MenuUser.java
+++ src/pl/coderslab/menu/MenuUser.java
@@ -4,140 +4,127 @@ import pl.coderslab.dao.UserDao;
 import pl.coderslab.plain.User;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuUser {
 
     public static void main() {
-        MenuMain menuMain = new MenuMain();
-        pl.coderslab.menu.MenuUser menuUser = new pl.coderslab.menu.MenuUser();
-        Scanner scanner = new Scanner(System.in);
+        int upperMenu = 4;
+        int lowerMenu = 1;
+        int properChoose = 0;
+        AdminPanel adminPanel = new AdminPanel();
+        MenuText menuText = new MenuText();
+        MenuUser menuUser = new MenuUser();
         UserDao user = new UserDao();
-        int choiceMenu = 4;
-
-        while (choiceMenu >= 1 || choiceMenu <= 5) {
-            System.out.println("**************************************");
-            System.out.println("************   Menu  User:  **********");
-            System.out.println("**************************************");
-            System.out.println("**** 1. Add - dodanie użytkownika    *");
-            System.out.println("**** 2. Edit - edycja użytkownika    *");
-            System.out.println("**** 3. Delete - kasacja użytkownika *");
-            System.out.println("**** 4. Quit - Main menu             *");
-            System.out.println("**************************************");
-
-            int choice = scanner.nextInt();
-            if (choice == 1) {
-                menuUser.add();
-            } else if (choice == 2) {
-                System.out.println(Arrays.toString(user.findAll()));
-                menuUser.edit();
-            } else if (choice == 3) {
-                System.out.println(Arrays.toString(user.findAll()));
-                menuUser.delete();
-            } else if (choice == 4) {
-                menuMain.main();
-            } else {
-                System.out.println("**********************************");
-                System.out.println("***** Podano błędną wartość! *****");
-                scanner.nextInt();
+        do {
+            try {
+                System.out.println(menuText.userWelcomeMenu());
+                Scanner scanner = new Scanner(System.in);
+                while (!scanner.hasNextInt()) {
+                    scanner.nextLine();
+                    System.err.println(menuText.wrongValue());
+                    System.out.println(menuText.mainWelcomeMenu());
+                    continue;
+                }
+                properChoose = scanner.nextInt();
+                if (properChoose < lowerMenu || properChoose > upperMenu) {
+                    System.err.println(menuText.wrongMenu());
+                    continue;
+                }
+                switch (properChoose) {
+                    case 1:
+                        menuUser.add();
+                        break;
+                    case 2:
+                        System.out.println(Arrays.toString(user.findAll()));
+                        menuUser.edit();
+                        break;
+                    case 3:
+                        System.out.println(Arrays.toString(user.findAll()));
+                        menuUser.delete();
+                        break;
+                    case 4:
+                        adminPanel.main();
+                        ;
+                    default:
+                        System.err.println(menuText.wrongMenu());
+                        break;
+                }
+            } catch (InputMismatchException ime) {
+                System.err.println(menuText.wrongValue());
                 continue;
             }
-        }
+        } while (properChoose > lowerMenu || properChoose < upperMenu);
     }
 
     public User add() {
         UserDao userDao = new UserDao();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("**************************************");
-        System.out.println("******* Tworzenie użytkownika: *******");
-        System.out.println("**************************************");
-        System.out.println("************* 0 = Wróć ***************");
-        System.out.println("****** Wpisz imię i użytkownika: *****");
-        System.out.println("**************************************");
+        MenuText menuText = new MenuText();
+        System.out.println(menuText.userCreateName());
         String name = scanner.nextLine();
         if (name.equals("0")) {
             MenuUser.main();
         }
-        System.out.println("**************************************");
-        System.out.println("*****  Wpisz email użytkownika:  *****");
-        System.out.println("**************************************");
+        System.out.println(menuText.userCreateMail());
+
         String email = scanner.nextLine();
         if (email.equals("0")) {
-            pl.coderslab.menu.MenuUser.main();
+            MenuUser.main();
         }
-        System.out.println("**************************************");
-        System.out.println("****  Wpisz password użytkownika: ****");
-        System.out.println("**************************************");
+
+        System.out.println(menuText.userCreatePass());
         String password = scanner.nextLine();
         if (password.equals("0")) {
-            pl.coderslab.menu.MenuUser.main();
+            MenuUser.main();
         }
-        System.out.println("***************************************");
-        System.out.println("*** Wpisz userGroupId użytkownika: ****");
-        System.out.println("***************************************");
+
+        System.out.println(menuText.userCreateGroup());
         int userGroupId = scanner.nextInt();
         if (userGroupId == 0) {
-            pl.coderslab.menu.MenuUser.main();
+            MenuUser.main();
         }
         User user = new User(name, email, password, userGroupId);
         userDao.create(user);
-        if (userDao.created(user.getId())) {
-            System.out.println("**********************************");
-            System.out.println("*******   User  create:   ********");
-            System.out.println("*******      Success     *********");
-            System.out.println("**********************************");
+        if (userDao.exist(user.getId())) {
+            System.out.println(menuText.userCreateSucc());
             return user;
         } else {
-            System.out.println("**********************************");
-            System.out.println("*******   User  create:   ********");
-            System.out.println("*******      Failed      *********");
-            System.out.println("**********************************");
+            System.out.println(menuText.userCreateFail());
             return null;
         }
     }
 
     public void edit() {
         Scanner scanner = new Scanner(System.in);
-        Scanner scanner1 = new Scanner(System.in);
+        MenuText menuText = new MenuText();
         User user = new User();
-        System.out.println("**************************************");
-        System.out.println("*******   Edycja użytkownika   *******");
-        System.out.println("*******  Wpisz ID użytkownika: *******");
-        System.out.println("**************************************");
-        System.out.println("************* 0 = Wróć ***************");
-        System.out.println("**************************************");
+        System.out.println(menuText.userEditId());
 
         int id = scanner.nextInt();
         if (id == 0) {
-            pl.coderslab.menu.MenuUser.main();
+            MenuUser.main();
         }
         UserDao userDao = new UserDao();
         System.out.println(userDao.read(id));
         scanner.nextLine();
-        System.out.println("**************************************");
-        System.out.println("****** Edytuj imię użytkownika: ******");
-        System.out.println("**************************************");
+        System.out.println(menuText.userEditName());
         String name = scanner.nextLine();
-        System.out.println("**************************************");
-        System.out.println("****** Edytuj email użytkownika: *****");
-        System.out.println("**************************************");
+        System.out.println(menuText.userEditMail());
         String email = scanner.nextLine();
         if (email.equals("0")) {
-            pl.coderslab.menu.MenuUser.main();
+            MenuUser.main();
         }
-        System.out.println("**************************************");
-        System.out.println("*****  Edytuj hasło użytkownika: *****");
-        System.out.println("**************************************");
+        System.out.println(menuText.userEditPass());
         String password = scanner.nextLine();
         if (password.equals("0")) {
-            pl.coderslab.menu.MenuUser.main();
+            MenuUser.main();
         }
-        System.out.println("***************************************");
-        System.out.println("*** Edytuj userGroupId użytkownika: ***");
-        System.out.println("***************************************");
+        System.out.println(menuText.userCreateGroup());
         int userGroupId = scanner.nextInt();
         if (userGroupId == 0) {
-            pl.coderslab.menu.MenuUser.main();
+            MenuUser.main();
         }
         user.setId(id);
         user.setName(name);
@@ -149,60 +136,38 @@ public class MenuUser {
 
     public void delete() {
         UserDao userDao = new UserDao();
+        MenuText menuText = new MenuText();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("*************************************");
-        System.out.println("*******  Usuwanie użytkownika *******");
-        System.out.println("* Podaj ID użytkownika do usunięcia *");
-        System.out.println("*************************************");
-        System.out.println("*********** 0 = Wróć ****************");
-        System.out.println("*************************************");
+        menuText.userDeleteId();
         while (scanner.hasNext()) {
             int id = scanner.nextInt();
             if (userDao.read(id) != null) {
                 System.out.println(userDao.read(id));
-                System.out.println("*******************************************************************");
-                 System.out.printf("********** Czy na pewno chcesz usunąć użytkownika o ID: %s ********\n", id);
-                System.out.println("********************** 1 = Tak ************************************");
-                System.out.println("********************** 2 = Nie ************************************");
-                System.out.println("*******************************************************************");
+                System.out.println(menuText.userDeleteConfirm());
                 int decision = scanner.nextInt();
                 while (scanner.hasNextLine()) {
                     if (decision == 1) {
                         userDao.delete(id);
                         if (userDao.read(id) == null) {
-                            System.out.println("**********************************");
-                            System.out.println("*******   User  delete:   ********");
-                            System.out.println("*******      Success     *********");
-                            System.out.println("**********************************");
-                            pl.coderslab.menu.MenuUser.main();
+                            System.out.println(menuText.userDeleteSucc());
+                            MenuUser.main();
                         } else {
-                            System.out.println("**********************************");
-                            System.out.println("*******   User  delete:   ********");
-                            System.out.println("*******      Failed      *********");
-                            System.out.println("**********************************");
-                            pl.coderslab.menu.MenuUser.main();
+                            System.err.println(menuText.userDeleteFail());
+                            MenuUser.main();
                         }
                     } else if (decision == 2) {
-                        pl.coderslab.menu.MenuUser.main();
+                        MenuUser.main();
                     } else {
-                        System.out.println("**********************************");
-                        System.out.println("*******    Wrong Value:   ********");
-                        System.out.println("********     Insert      *********");
-                        System.out.println("**********************************");
+                        System.err.println(menuText.wrongValue());
                     }
                 }
 
             } else {
-                System.out.println("***********************************");
-                System.out.println("*********    User Error:   ********");
-                System.out.println("*********    No User ID    ********");
-                System.out.println("*********  Write ID again  ********");
-                System.out.println("*********  Press 0 to back ********");
-                System.out.println("***********************************");
+                System.err.println(menuText.userNoIdError());
                 continue;
             }
             if (id == 0) {
-                pl.coderslab.menu.MenuUser.main();
+                MenuUser.main();
             } else {
                 continue;
             }
